92dc888f07fbad34c1e34c4cd40a580b974d257b
Ines <stigracer666@gmail.com>
2022-02-23 10:51:00 +0100

    Cambios en la escena de opciones

--- PR_ZAXXON_GarciaCabreroInes/Assets/Scripts/Music.cs
+++ PR_ZAXXON_GarciaCabreroInes/Assets/Scripts/Music.cs
@@ -7,6 +7,7 @@ public class Music : MonoBehaviour
     private AudioSource thiefMusic;
     private GameObject[] other;
     private bool NotFirst = false;
+   
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +18,7 @@ public class Music : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void Awake()
